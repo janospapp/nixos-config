29705d481b83485fa0d4550bf1165638c5bb8d0a
Janos Papp <papp.janos.90@gmail.com>
2023-08-05 22:59:35 +0200

    Update Time Zone

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -26,6 +26,8 @@
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
+  time.timeZone = "Europe/Budapest";
+
   # Select internationalisation properties.
   # i18n.defaultLocale = "en_US.UTF-8";
   # console = {
