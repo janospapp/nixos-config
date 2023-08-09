d2aab9d40881f9d8e06393c2c6e2adabc81c45fb
Janos Papp <papp.janos.90@gmail.com>
2023-08-10 01:55:55 +0200

    Add zsh configuration

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -14,5 +14,54 @@
       userName = "Janos Papp";
       userEmail = "papp.janos.90@gmail.com";
     };
+
+    zsh = {
+      enable = true;
+      enableAutosuggestions = true;
+      enableSyntaxHighlighting = true;
+      defaultKeymap = "viins";
+
+      shellAliases = {
+        k = "kubectl";
+        dcu = "docker-compose up -d";
+        dcd = "docker-compose down";
+        bopen = "bundle open";
+        bclean = "bundle pristine";
+      };
+
+      initExtra = ''
+        zstyle ':completion:*' menu select
+        
+        bindkey -M viins '^l' end-of-line
+
+        zmodload zsh/complist
+        bindkey -M menuselect '^h' vi-backward-char
+        bindkey -M menuselect '^j' vi-down-line-or-history
+        bindkey -M menuselect '^k' vi-up-line-or-history
+        bindkey -M menuselect '^l' vi-forward-char
+        bindkey -M menuselect '\t' accept-line
+        
+        setopt autocd nomatch notify
+        unsetopt beep
+      '';
+
+      plugins = [
+        {
+          name = "vi-mode";
+          src = pkgs.zsh-vi-mode;
+          file = "share/zsh-vi-mode/";
+        }
+        {
+          name = "powerlevel10k";
+          src = pkgs.zsh-powerlevel10k;
+          file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+        }
+        {
+          name = "powerlevel10k-config";
+          src = ./p10k-config;
+          file = "p10k.zsh";
+        }
+      ];
+    };
   };
 }
--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -76,7 +76,7 @@
 
   programs.zsh.enable = true;
 
-  # # Define a user account. Don't forget to set a password with ‘passwd’.
+  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.janos = {
     isNormalUser = true;
     initialPassword = "P@ssw0rd";
